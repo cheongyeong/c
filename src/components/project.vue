b15ece72917fc6a830fafc1ea1dc295b6e5b6d51
cheongyeong <cheongyeonggg@gmail.com>
2023-01-26 17:47:23 +0900

    edit

--- src/components/project.vue
+++ src/components/project.vue
@@ -1,4 +1,10 @@
 <template>
+<div class="projectBg" v-if="antra == true">
+     <img class="closeBtn" src="img/close-white.png" alt="" @click="antra = false">
+     <div  class="Popup">
+       <img src="img/antra.png" alt="">
+     </div>
+</div>
 
 <section class="project-section">
       <h1 class="bold">PROJECT</h1>
@@ -18,22 +24,32 @@
          <div class="more" @click="antra = true">more view</div>
        </div>
      </div>
-    </div>
-  <div v-show="currentTab == 1" class="tab-cont">
-  <div class="projectTitle">
-    <h3>project.02</h3>
-    <h2>서울탁주<br>반응형 웹사이트 리뉴얼</h2>
-    <div class="more">more view</div>
-  </div></div>
-  <div v-show="currentTab == 2" class="tab-cont">
-  <div class="projectTitle">
-    <h3>project.03</h3>
-    <h2>서문시장야시장<br>반응형 웹사이트 리뉴얼</h2>
-    <div class="more">more view</div>
-  </div></div>
+
+     <div v-show="currentTab == 1" class="tab-cont seoulTakju">
+      <div class="projectTitle">
+         <h3>project.02</h3>
+         <h2>서울탁주<br>반응형 웹사이트 리뉴얼</h2>
+         <div class="more">more view</div>
+      </div>
+       <img class="bori" src="img/move-bori.gif" alt="">
+     </div>
+
+     <div v-show="currentTab == 2" class="tab-cont">
+       <div class="projectTitle">
+         <h3>project.03</h3>
+         <h2>서문시장야시장<br>반응형 웹사이트 리뉴얼</h2>
+         <div class="more">more view</div>
+      </div>
+  </div>
+
+  </div>
+
+
   </div>
 
 </section>
+
+<span></span>
 
   
 </template>
