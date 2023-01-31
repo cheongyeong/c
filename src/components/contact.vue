413af6700c79c4384d8fcbb0c3a8ef0ac94ce513
cheongyeong <cheongyeonggg@gmail.com>
2023-01-31 11:57:27 +0900

    수정

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,67 +1,65 @@
 <template>
     <section class="contactMe-section" id="contact">
-      <ul class="city-buttons">
-  	   <li 
-       @click="selectCity"
-       :class="{ isSelected: citySelected === true }"
-       v-for="city in cities" :key="city.code">
-        {{ cities.lable }}
-       </li>
-      </ul>
 
       <h1 class="bold">CONTACT ME</h1>
       <h2>지금까지 저의 포트폴리오를 봐주셔서 감사합니다.</h2>
 
+
       <div class="contact-wrap">
-        <div class="mail"  @click="toggleOnOff">
-           <span>
-             <h2 class="bold">Mail</h2>
+        <div class="mail">
+           <span class="bold mails" @click="show = !show" :class="{active:show}">
+             Mail
            </span>
-           <h3  v-if="isStatusOn">cheongyeonggg@gmail.com</h3>
+             <Transition>
+              <p v-if="show">cheongyeonggg@gmail.com</p>
+            </Transition>
         </div>
         <div class="mobile">
-          <span>
-            <h2 class="bold">Mobile</h2>
-          </span>
+           <span class="bold mobiles" @click="show = !show" :class="{active:show}">
+             Mobile
+           </span>
+             <Transition>
+              <p v-if="show">cheongyeonggg@gmail.com</p>
+            </Transition>
         </div>
-        <div class="insta">
-          <span>
-            <h2 class="bold">GitHub</h2>
-          </span>
+
+        <div class="GitHub">
+           <span class="bold" @click="show= !show" :class="{active:show}">
+             GitHub
+           </span>
+             <Transition>
+              <p v-if="show">cheongyeonggg@gmail.com</p>
+            </Transition>
         </div>
+       
+    
       </div>
+
+
+
+
+
+
+
+
 
     </section>
   
 </template>
 
 <script>
-export default {
-  name: 'city-selector',
-      data(){
-  return {
-    isStatusOn: false,
-    cities: [
-        { code: "Seoul", label: "서울", selected: false},
-        { code: "London", label: "런던", selected: false},
-        { code: "Chicago", label: "시카고", selected: false},
-      ]
-}
-},
 
-methods: {
-  selectCity(item) {
-    item.selected = !item.selected;
+export default {
+  data() {
+    return {
+      show: true,
+
+
+    }
   },
 
-  toggleOnOff: function() {
-    this.isStatusOn = !this.isStatusOn;
-    
-  }, 
-}
-}
 
-
+}
 
 
 </script>
@@ -69,6 +67,15 @@ methods: {
 <style scoped>
 
 
+.v-enter-from,
+.v-leave-to {
+  opacity: 1;
+}
 
-  
+.active{
+  left: 200px;
+  transition: all 0.5s ;
+}
+
+
 </style>
