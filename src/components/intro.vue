62ff8d917eac3eee4e4174f8bd7fafbff38a1e68
cheongyeong <cheongyeonggg@gmail.com>
2023-02-01 00:53:26 +0900

    수정

--- src/components/intro.vue
+++ src/components/intro.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="intro-section">
       <h1>안녕하세요.<br>
-        <span class="blue bold">다채로운</span><br>
+        <span class="blue bold">프론트엔드</span><br>
         <span class="bold">개발자</span><br>
         박천경입니다.</h1>
       <div class="bg-circle">
