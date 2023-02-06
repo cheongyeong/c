e8ea0934161de05cd54330f0a81f77622fbf49ce
cheongyeong <cheongyeonggg@gmail.com>
2023-02-07 02:37:15 +0900

    .

--- src/components/project.vue
+++ src/components/project.vue
@@ -5,6 +5,18 @@
        <img src="img/antra.png" alt="">
      </div>
 </div>
+<div class="projectBg" v-if="takju == true">
+     <img class="closeBtn" src="img/close-white.png" alt="" @click="takju = false">
+     <div  class="Popup">
+       <img src="img/takju.png" alt="">
+     </div>
+</div>
+<div class="projectBg" v-if="seomun == true">
+     <img class="closeBtn" src="img/close-white.png" alt="" @click="seomun = false">
+     <div  class="Popup">
+       <img src="img/seomun.png" alt="">
+     </div>
+</div>
 
 <section class="project-section" id="project">
       <h1 class="bold">PROJECT</h1>
@@ -24,14 +36,22 @@
       <div v-show="currentTab == 0" class="tab-cont antra">
        <div class="projectTitle">
          <h2>앤트러사이트 커피<br>반응형 웹사이트 리뉴얼</h2>
-         <div class="more" @click="antra = true">more view</div>
-       </div>
+         <div class="projectBtn">
+           <div class="more" @click="antra = true">Mockup</div>
+           <div class="more"><a target="_blank" href="https://github.com/cheongyeong/Anthracite-Coffee.">Git Hub</a></div>
+           <div class="more"><a target="_blank" href="">Web Page</a></div>
+         </div>
+        </div>
      </div>
 
      <div v-show="currentTab == 1" class="tab-cont seoulTakju">
       <div class="projectTitle">
          <h2>서울탁주<br>반응형 웹사이트 리뉴얼</h2>
-         <div class="more">more view</div>
+         <div class="projectBtn">
+           <div class="more" @click="takju = true">Mockup</div>
+           <div class="more"><a target="_blank" href="https://github.com/cheongyeong/seoulTakju">Git Hub</a></div>
+           <div class="more"><a target="_blank" href="https://seoultakjju.herokuapp.com/">Web Page</a></div>
+         </div>
       </div>
        <img class="bori" src="img/move-bori.gif" alt="">
      </div>
@@ -39,7 +59,11 @@
      <div v-show="currentTab == 2" class="tab-cont seomun" >
        <div class="projectTitle">
          <h2>서문시장야시장<br>반응형 웹사이트 리뉴얼</h2>
-         <div class="more">more view</div>
+         <div class="projectBtn">
+           <div class="more" @click="seomun = true">Mockup</div>
+           <div class="more"><a target="_blank" href="https://github.com/cheongyeong/seomunNightMarket">Git Hub</a></div>
+           <div class="more"><a target="_blank" href="">Web Page</a></div>
+         </div>
       </div>
   </div>
 
@@ -67,9 +91,12 @@ export default {
         'project.03'
       ],
       antra : false,
-    }
+      seomun : false,
+      takju : false
+  
     
   }
+}
 }
 </script>
 
